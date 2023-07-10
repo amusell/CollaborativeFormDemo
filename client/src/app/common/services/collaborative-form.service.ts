801955e3348ac2b95f70fecfe7ad14617380d470
Marco Ricupero <m.ricupero@avanade.com>
2023-07-11 01:10:19 +0200

    rev dependency

--- client/src/app/common/services/collaborative-form.service.ts
+++ client/src/app/common/services/collaborative-form.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CollaborativeForm } from '../../../../../collaborativeForm/client';
+import { CollaborativeForm } from 'collaborative-form-client';
 
 @Injectable({ providedIn: 'root' })
 export class CollaborativeFormService {
